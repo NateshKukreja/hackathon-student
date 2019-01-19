ec5c0b8d1aeb8a50588d8073c9d7ef4e8ba9d50a
nateshkukreja <natesh.kukreja@gmail.com>
2019-01-18 23:04:53 -0500

    Creating new components

---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import Image from '../image'
+
+const Footer = () => {
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+}
+
+export default Footer
